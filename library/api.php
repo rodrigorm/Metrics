d1a8cd2d42cc25cee25731d514b806f339e7ce4b
Rodrigo Moyle <rodrigo.moyle@dafiti.com.br>
2013-09-12 20:50:09 -0300

    Added velocity monitoring to server.

--- library/api.php
+++ library/api.php
@@ -5,9 +5,5 @@ function metric_collect($target, $value, $timestamp = 'now')
     $message = sprintf('%s %s %s', $target, $value, date('U', strtotime($timestamp)));
 
     $socket = fsockopen('udp://localhost', 2003);
-    try {
-        fwrite($socket, $message);
-    } catch (Exception $e) {
-        // ignore errors
-    }
+    fwrite($socket, $message);
 }
